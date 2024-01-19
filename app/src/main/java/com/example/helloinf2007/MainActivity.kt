12c0da282074eaaa3a4dab89b9482da29df9a108
Zanaqi <2837305m@student.gla.ac.uk>
2024-01-19 22:43:22 +0800

    change

--- app/src/main/java/com/example/helloinf2007/MainActivity.kt
+++ app/src/main/java/com/example/helloinf2007/MainActivity.kt
@@ -38,7 +38,7 @@ class MainActivity : ComponentActivity() {
                 ) {
                     Column {
                         Greeting("INF2007")
-                        From("Michael")
+                        From("Mirza")
                     }
                 }
             }
